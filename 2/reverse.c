15d762e41bf0b5572c6b1671b7baac7e60f59782
khalili <khalili@asb9838nu-c06.csil.sfu.ca>
2016-09-23 10:30:15 -0700

    reverse is working

---
+++ 2/reverse.c
@@ -0,0 +1,11 @@
+void reverse( int arr[], unsigned int len ){
+  int i = 0;
+  int tmp = 0;
+  for(i; i<(len/2); i++){
+    tmp = arr[i];
+    arr[i] = arr[len-i-1];
+    arr[len-i-1]=tmp;
+
+  }
+  return;
+}
