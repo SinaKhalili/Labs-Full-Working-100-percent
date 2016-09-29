ecac48ad58a26d42d7964ccc83d8f35fc5793237
khalili <khalili@asb9804u-d01.csil.sfu.ca>
2016-09-29 10:37:24 -0700

    I swear it works now

--- 2/count.c
+++ 2/count.c
@@ -4,16 +4,19 @@ int main () {
   int character = 0;
   int words = 0;
   int lines = 0;
+  int d = 0;
   int c = getchar();
   while( c != -1){
     character++;
     if(!((c > 64 && c<91)||(c > 96 && c<123)||(c == 39))){
-      //do new lines count as words? maybe?
+      if ((d > 64 && d<91)||(d > 96 && d<123)||(d == 39)){
       words++;
     }
+  }
     if(c == 10){
       lines++;
     }
+    d = c;
     c = getchar();
   }
   printf("%lu %lu %lu\n", character, words, lines );
