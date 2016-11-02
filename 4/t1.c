c20b370d579fb04121633652a9091ffaa6c14ab2
khalili <khalili@asb9838nu-d14.csil.sfu.ca>
2016-11-02 16:37:59 -0700

    So turns out the abs function is a hunk of JUNK

--- 4/t1.c
+++ 4/t1.c
@@ -6,17 +6,24 @@ void draw_circle( uint8_t img[],
 		              int y,
 		              int r,
 		              uint8_t color ){
-          float Xdistance = 0;
-          float Ydistance = 0;
+
           int a = 0;
           int b = 0;
           for (b = 0; b<rows; b++){
             for(a = 0; a<cols; a++){
-              Xdistance = abs(a-x);
-              Ydistance = abs(b-y);
-
+              float Xdistance1 = (a-0.5)-(x);
+              float Xdistance2 = (a+0.5)-(x);
+              float Xdistance3 = (a-0.5)-(x);
+              float Xdistance4 = (a+0.5)-(x);
+              float Ydistance1 = (b+0.5)-(y);
+              float Ydistance2 = (b+0.5)-(y);
+              float Ydistance3 = (b-0.5)-(y);
+              float Ydistance4 = (b-0.5)-(y);
               if(
-                  round(sqrt((Xdistance*Xdistance)+(Ydistance*Ydistance))) < r
+                  (Xdistance2*Xdistance2)+(Ydistance2*Ydistance2) < (r*r) ||
+                  (Xdistance1*Xdistance1)+(Ydistance1*Ydistance1) < (r*r) ||
+                  (Xdistance3*Xdistance3)+(Ydistance3*Ydistance3) < (r*r) ||
+                  (Xdistance4*Xdistance4)+(Ydistance4*Ydistance4) < (r*r)
               ){
                 img[b*cols + a] = color;
               }
