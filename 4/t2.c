14a200e3c02709c72393010ce572fd8c01a87d89
khalili <khalili@asb9840u-e03.csil.sfu.ca>
2016-11-06 16:55:16 -0800

    The beginning of the end of the end of the beginning

--- 4/t2.c
+++ 4/t2.c
@@ -7,30 +7,40 @@ void draw_rectangle( uint8_t array[],
 		          int rect_width,
 		          int rect_height,
 		          uint8_t color ){
+
+      if(rect_width == 0 ||rect_height == 0){return;}
+
       if (rect_height<0 && rect_width<0){
-        printf("both negatives!\n");
-        printf("x used to be %d ", x);
-        x += rect_width+1;
-        y += rect_height+1;
-        printf("and now its %d\n", x);
+        /*printf("both negatives!\n");
+        printf("x used to be %d ", x);*/
+        x++;
+        x += rect_width;
+        y++;
+        y += rect_height;
+        //printf("and now its %d\n", x);
         draw_rectangle(array, cols, rows, x,y, abs(rect_width),abs(rect_height), color );
+        return;
 
       }
       else if (rect_width<0 && rect_height>0){
         //printf("width negative!\n");
-        x += rect_width+1;
+        x++;
+        x += rect_width;
         draw_rectangle(array, cols, rows, x,y, abs(rect_width),abs(rect_height), color );
+        return;
       }
       else if (rect_height<0 && rect_width>0){
         //printf("height negative!\n");
-        y += rect_height+1;
+        y++;
+        y += rect_height;
         draw_rectangle(array, cols, rows, x,y, abs(rect_width),abs(rect_height), color );
+        return;
       }
       int i = y;
       int k = x;
 
       if(rect_height > 0 && rect_width > 0 ){
-      for(i = y; i<=(y + rect_height); i++){
+      for(i = y; i<(y + rect_height); i++){
 
         if(k>=0 && i>=0){
           array[i*cols + x] = color;
@@ -38,7 +48,7 @@ void draw_rectangle( uint8_t array[],
 
         for (k = x; k <x+rect_width-1; k++){
 
-          if( i == y || i == (y+rect_height)){
+          if( i == y || i == (y+rect_height)-1){
 
             if(k>=0 && i>=0){
               array[i*cols+k] = color;
@@ -55,7 +65,4 @@ void draw_rectangle( uint8_t array[],
         k=x;
       }
     }
-
-
-
 }
